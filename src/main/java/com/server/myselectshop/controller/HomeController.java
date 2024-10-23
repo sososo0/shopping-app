5358c2d47bca2d79d9c564bbd838bfe621bc2081
sohyun <sh0000@pusan.ac.kr>
2024-10-23 15:58:26 +0900

    chore: add index page environment and database dialect configuration

---
+++ src/main/java/com/server/myselectshop/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.server.myselectshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
