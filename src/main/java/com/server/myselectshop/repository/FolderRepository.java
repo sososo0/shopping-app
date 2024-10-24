0ee2f0c2889bd9f10d66d85d88f46641c55833e7
sohyun <sh0000@pusan.ac.kr>
2024-10-24 21:05:24 +0900

    feat: implement product folder architecture

---
+++ src/main/java/com/server/myselectshop/repository/FolderRepository.java
@@ -0,0 +1,7 @@
+package com.server.myselectshop.repository;
+
+import com.server.myselectshop.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+}
---
+++ src/main/java/com/server/myselectshop/repository/ProductFolderRepository.java
@@ -0,0 +1,7 @@
+package com.server.myselectshop.repository;
+
+import com.server.myselectshop.entity.ProductFolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductFolderRepository extends JpaRepository<ProductFolder, Long> {
+}
