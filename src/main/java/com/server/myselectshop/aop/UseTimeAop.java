322cdd9f49a9119865001e1d064855e897c0af3e
sohyun <sh0000@pusan.ac.kr>
2024-10-27 18:13:50 +0900

    feat: apply spring aop

---
+++ src/main/java/com/server/myselectshop/aop/UseTimeAop.java
@@ -0,0 +1,74 @@
+package com.server.myselectshop.aop;
+
+import com.server.myselectshop.entity.ApiUseTime;
+import com.server.myselectshop.entity.User;
+import com.server.myselectshop.repository.ApiUseTimeRepository;
+import com.server.myselectshop.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Slf4j(topic = "UseTimeAop")
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class UseTimeAop {
+
+    private final ApiUseTimeRepository apiUseTimeRepository;
+
+    @Pointcut("execution(* com.server.myselectshop.controller.ProductController.*(..))")
+    public void product() {}
+
+    @Pointcut("execution(* com.server.myselectshop.controller.FolderController.*(..))")
+    public void folder() {}
+
+    @Pointcut("execution(* com.server.myselectshop.naver.controller.NaverApiController.*(..))")
+    public void naver() {}
+
+    @Around("product() || folder() || naver()")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        // 측정 시작 시각
+        long startTime = System.currentTimeMillis();
+
+        try {
+            // 핵심 기능 수행
+            Object output = joinPoint.proceed();
+            return output;
+        } finally {
+            // 측정 종료 시각
+            long endTime = System.currentTimeMillis();
+            // 수행 시간 = 종료 시각 - 시작 시각
+            long runTime = endTime - startTime;
+
+            // 로그인 회원이 없는 경우, 수행 시간 기록하지 않음
+            Authentication auth = SecurityContextHolder
+                    .getContext()
+                    .getAuthentication();
+            if (auth != null && auth.getPrincipal().getClass() == auth.getPrincipal()) {
+                // 로그인 회원 정보
+                UserDetailsImpl userDetails = (UserDetailsImpl) auth.getPrincipal();
+                User loginUser = userDetails.getUser();
+
+                // API 사용시간 및 DB에 기록
+                ApiUseTime apiUseTime = apiUseTimeRepository.findByUser(loginUser).orElse(null);
+                if (apiUseTime == null) {
+                    // 로그인 회원의 기록이 없으면
+                    apiUseTime = new ApiUseTime(loginUser, runTime);
+                } else {
+                    // 로그인 회원의 기록이 있으면
+                    apiUseTime.addUseTime(runTime);
+                }
+
+                log.info("[API Use Time] Username: " + loginUser.getUsername()
+                        + ", Total Time: " + apiUseTime.getTotalTime() + " ms");
+                apiUseTimeRepository.save(apiUseTime);
+            }
+        }
+    }
+}
